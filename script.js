04c0f0d67b0952dc63eec671ac0639b78afacb50
smRid <omorsk911@gmail.com>
2025-03-29 12:26:51 +0600

    JS update

--- script.js
+++ script.js
@@ -100,7 +100,7 @@ slideshow()
 const section3Content = document.querySelector('.section-3-content')
 
 window.addEventListener('scroll', () => {
-    if(window.pageYOffset + window.innerHeight >= section3Content.offsetTop + section3Content.offsetHeight / 2) {
+    if(window.pageYOffset + window.innerHeight >= section3Content + section3Content.offsetHeight / 2) {
         section3Content.classList.add('change')
     }
 })
