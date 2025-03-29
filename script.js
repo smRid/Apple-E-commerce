634d3407324e514887e855f51bd5da37e7525062
smRid <omorsk911@gmail.com>
2025-03-29 12:25:56 +0600

    Section 3 part added

--- script.js
+++ script.js
@@ -93,3 +93,15 @@ const slideshow = () => {
 
 slideshow()
 // End of Slideshow
+// End of section 1
+
+
+// Section 3
+const section3Content = document.querySelector('.section-3-content')
+
+window.addEventListener('scroll', () => {
+    if(window.pageYOffset + window.innerHeight >= section3Content.offsetTop + section3Content.offsetHeight / 2) {
+        section3Content.classList.add('change')
+    }
+})
+// End of Section 3
